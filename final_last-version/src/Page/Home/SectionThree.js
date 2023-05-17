461e8fc3df3d2aef2d7fd3275a304b41b6626682
Mohamad Alkaadi <mohamadalkaadi@gmail.com>
2023-05-17 14:12:50 +0300

    set window size in react

--- final_last-version/src/Page/Home/SectionThree.js
+++ final_last-version/src/Page/Home/SectionThree.js
@@ -6,7 +6,16 @@ const img = ()=>{
   return <img src={secThee} draggable="false" />;
 }
 
-
+function useWindowSize(){
+  const [size,setSize]=useState(window.innerWidth);
+  useEffect(()=>{
+    const handleResize = () => {
+    setSize(window.innerWidth);
+    };
+    window.addEventListener('resize',handleResize);
+  },[]);
+  return size;
+}
 
 
 
@@ -15,7 +24,9 @@ const hellobe = (buttonValue,buttonActive) =>{
 }
 
 const SectionThree = () => {
-  const [hello,setHello] =useState({value:0});
+  const [hello,setHello] =useState({value:0,forSmallScreen:false});//for small screen for activating button and closing it
+
+  const width = useWindowSize();
 
   const buttonOne = () =>{
     setHello({...hello,value:1});
@@ -57,11 +68,11 @@ const SectionThree = () => {
         return <img src={secThee} draggable="false" />
         break;
       case 1:
-        return(<div className='hidden-content-one hidden-content'>
+        return(<div className='hidden-content-one hidden-content small-screen-content'>
                 <button onClick={backClick} className='back-button'><MdClose/></button>
                 <div className='hidden-para'>
                   <p className='hidden-title'>Monitor patients continuously and remotely</p>
-                  <p className='hidden-para'>One of the challenges in healthcare is to monitor patients continuously and remotely without compromising their comfort and mobility. A possible solution is to use a watch that can measure vital signs such as heart rate, blood pressure, oxygen saturation, and temperature, and transmit the data to a cloud-based platform where doctors can access it anytime and anywhere. 
+                  <p className='hidden-text'>One of the challenges in healthcare is to monitor patients continuously and remotely without compromising their comfort and mobility. A possible solution is to use a watch that can measure vital signs such as heart rate, blood pressure, oxygen saturation, and temperature, and transmit the data to a cloud-based platform where doctors can access it anytime and anywhere. 
                     This way, patients can enjoy their daily activities while being under medical supervision, and doctors can intervene promptly if any abnormality is detected.
                 </p>
                </div>
@@ -69,53 +80,53 @@ const SectionThree = () => {
         break;
       case 2:
         return(
-                <div className='hidden-content-two hidden-content'>
+                <div className='hidden-content-two hidden-content small-screen-content'>
                   <button onClick={backClick} className='back-button'><MdClose/></button>
                   <div className='hidden-para'>
 
                     <p className='hidden-title'>Easy booking of same or next day appointments</p>
-                    <p className='hidden-para'> 
+                    <p className='hidden-text'> 
                     One of the greatest challenges of caring for an elderly loved one is ensuring their safety and well-being when you are not around. You want to respect their independence and privacy, but you also worry about their health and comfort. That’s why our company offers a unique service that monitors the vital signs of our clients 24/7, using a discreet and comfortable device that they wear on their wrist. This way, you can have peace of mind knowing that they are always under professional supervision, and that you will be alerted immediately if there is any sign of trouble.
                      </p>
                 </div>
                </div>)
         break;
       case 3:
-        return(<div className='hidden-content-three hidden-content'>
+        return(<div className='hidden-content-three hidden-content small-screen-content'>
                   <button onClick={backClick} className='back-button'><MdClose/></button>
                   <div className='hidden-para'>
                    <p className='hidden-title'>Peace of mind for family members</p>
-                     <p p className='hidden-para'>Our company offers a service that helps elderly people stay healthy and independent at home. With our service, they get a wearable device that measures their vital signs and sends the data to a cloud platform. This allows their family members or health care professionals to access the data and monitor their health status. The device also detects any abnormalities and alerts the nearest hospital in case of an emergency. Our service connects the elderly to their loved ones and health care team, while enabling them to live comfortably at home. It also lowers the risk of complications and hospitalizations. </p>
+                     <p p className='hidden-text'>Our company offers a service that helps elderly people stay healthy and independent at home. With our service, they get a wearable device that measures their vital signs and sends the data to a cloud platform. This allows their family members or health care professionals to access the data and monitor their health status. The device also detects any abnormalities and alerts the nearest hospital in case of an emergency. Our service connects the elderly to their loved ones and health care team, while enabling them to live comfortably at home. It also lowers the risk of complications and hospitalizations. </p>
                 </div>
                </div>)
         break;
       case 4:
-        return(<div className='hidden-content-four hidden-content'>
+        return(<div className='hidden-content-four hidden-content small-screen-content'>
                 <button onClick={backClick} className='back-button'><MdClose/></button>
                   <div className='hidden-para'>
                    <p className='hidden-title'>Easier access to patient data remotely</p>
-                     <p p className='hidden-para'>
+                     <p p className='hidden-text'>
                         One of the benefits of our service is that it allows easier access to patient data remotely. This means that doctors can view the vital signs and medical history of their elderly patients anytime and anywhere, without having to visit them in person. This can save time, reduce costs, and improve the quality of care. By having a comprehensive and up-to-date picture of the patient's health, doctors can make better decisions and intervene promptly if needed.
                       </p>
                 </div>
                </div>)
         break;
       case 5:
-        return(<div className='hidden-content-five hidden-content'>
+        return(<div className='hidden-content-five hidden-content small-screen-content'>
                <button onClick={backClick} className='back-button'><MdClose/></button>
                   <div className='hidden-para'>
                    <p className='hidden-title'>Personalized and flexible services</p>
-                     <p p className='hidden-para'>
+                     <p p className='hidden-text'>
                      Our company offers personalized and flexible services for elderly care, based on monitoring their vital signs and connecting them to the nearest hospital that is contracted with us. We have three plans to suit different needs and budgets: basic, standard and premium. The basic plan has a low monthly subscription fee, but a higher cost when you need help. The standard plan has a moderate monthly fee and a moderate cost when you need help. The premium plan has a high monthly fee, but a low or no cost when you need help. You can choose the plan that best fits your situation and preferences. </p>
                 </div>
                </div>)
         break;
       case 6:
-        return(<div className='hidden-content-six hidden-content'>
+        return(<div className='hidden-content-six hidden-content small-screen-content'>
                 <button onClick={backClick} className='back-button'><MdClose/></button>
                   <div className='hidden-para'>
                    <p className='hidden-title'>Provide remote care and guidance to patients</p>
-                     <p p className='hidden-para'>
+                     <p p className='hidden-text'>
                      Our company provides remote care and guidance to patients who need frequent monitoring of their vital signs. We use a device that connects patients with a team of health professionals who can offer advice and support. We also link patients with the nearest hospital that is contracted with us, in case of emergencies. Our service is convenient, safe and reliable for patients who want to stay at home and avoid unnecessary hospital visits. </p>
                 </div>
                </div>)
@@ -148,6 +159,9 @@ const SectionThree = () => {
                     Monitor patients continuously and remotely
                   </button>
                 </div>
+                {hello.forSmallScreen ? <div className='small-screen-text'>
+                  {secTwo}
+                </div> : null}
 
                 <div className='sub-ben text-two'>
                   <button onClick={buttonTwo} className='sub-ben-text'>
@@ -206,7 +220,7 @@ const SectionThree = () => {
           </div>
           
           {hellobe(hello.value)}
-          {secTwo(hello.value)}
+          {width>464?secTwo(hello.value):null}
         </div>
         
     </div>

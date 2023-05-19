39177fc135ad78372f9a3e8ccfe6cd8db76593fd
Mohamad Alkaadi <mohamadalkaadi@gmail.com>
2023-05-19 22:23:43 +0300

    finished  section6 but didn't change its name to 5

--- final_last-version/src/Page/Home/SectionSix.js
+++ final_last-version/src/Page/Home/SectionSix.js
@@ -1,10 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState,useRef,useEffect } from 'react'
 import './home-css/sectionsix.css'
+import {motion} from 'framer-motion'
+
+
+
+
+
+
 const SectionSix = () => {
     const [timePeriod,setTimePeriod] = useState(false);
     const handleChange = () =>{
         setTimePeriod(!timePeriod);
     }
+    
+    const[width, setWidth] = useState(0);
+  const carousel = useRef();
+
+  useEffect(() =>{
+    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+  },[]);
+
+
   return (
     <div className='section-six-container'>
         <div className='section-six grid-system'>
@@ -21,7 +37,7 @@ const SectionSix = () => {
             </div>
 
             <div className='six-cards'>
-                <div className='six-card six-card-one'>
+                <div className='six-normal six-card six-card-one'>
                     <div className='six-card-content six-card-header'>Basic</div>
                     <div className='six-card-content six-card-price'>
                         {!timePeriod?<>200.000SYP</>:<>2.200.000SYP</>}
@@ -32,7 +48,7 @@ const SectionSix = () => {
                     <button className='sec-six-btn'>Learn More</button>
                 </div>
 
-                <div className='six-card six-card-two'>
+                <div className='six-normal six-card six-card-two'>
                     <div className='six-card-content six-card-header'>Standard</div>
                     <div className='six-card-content six-card-price'>
                     {!timePeriod?<>250.000SYP</>:<>2.700.000SYP</>}
@@ -43,7 +59,7 @@ const SectionSix = () => {
                     <button className='sec-six-btn'>Learn More</button>
                 </div>
 
-                <div className='six-card six-card-three'>
+                <div className='six-normal six-card six-card-three'>
                     <div className='six-card-content six-card-header'>Premium</div>
                     <div className='six-card-content six-card-price'>
                     {!timePeriod?<>300.000SYP</>:<>3.000.000SYP</>}
@@ -54,6 +70,47 @@ const SectionSix = () => {
                     </div>
                     <button className='sec-six-btn'>Learn More</button>
                     </div>
+                    {/*------------------------- slider --------------------------------*/}
+
+
+            <motion.div ref={carousel} whileTap={{cursor:"grabbing"}} className='six-carousel'>
+              <motion.div drag="x" dragConstraints={{right:0,left: -width}} className='six-inner-carousel'>
+                <motion.div className='six-slide six-item six-card six-card-one'>
+                    <div className='six-card-content six-card-header'>Basic</div>
+                    <div className='six-card-content six-card-price'>
+                        {!timePeriod?<>200.000SYP</>:<>2.200.000SYP</>}
+                    </div>
+                    <div className='six-card-content six-card-benefits'>
+                        pay less every month, but more when you need help
+                    </div>
+                    <button className='sec-six-btn'>Learn More</button>
+                </motion.div>
+
+
+                <motion.div className='six-slide six-item six-card six-card-two'>
+                    <div className='six-card-content six-card-header'>Standard</div>
+                    <div className='six-card-content six-card-price'>
+                    {!timePeriod?<>250.000SYP</>:<>2.700.000SYP</>}
+                    </div>
+                    <div className='six-card-content six-card-benefits'>
+                        pay a moderate amount every month, and a moderate amount when you need help
+                    </div>
+                    <button className='sec-six-btn'>Learn More</button>
+                </motion.div>
+                <motion.div className='six-slide six-item six-card six-card-three'>
+                <div className='six-card-content six-card-header'>Premium</div>
+                    <div className='six-card-content six-card-price'>
+                    {!timePeriod?<>300.000SYP</>:<>3.000.000SYP</>}
+
+                    </div>
+                    <div className='six-card-content six-card-benefits'>
+                        pay more every month, but less or nothing when you need help
+                    </div>
+                    <button className='sec-six-btn'>Learn More</button>
+                </motion.div>
+              </motion.div>
+            </motion.div>
+
 
             </div>
         </div>

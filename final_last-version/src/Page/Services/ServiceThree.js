5f4bcc55de078e5b969936367986d1c255e5f57c
Mohamad Alkaadi <mohamadalkaadi@gmail.com>
2023-05-29 16:32:10 +0300

    ser-three structure

--- final_last-version/src/Page/Services/ServiceThree.js
+++ final_last-version/src/Page/Services/ServiceThree.js
@@ -5,7 +5,16 @@ const ServiceThree = () => {
   return (
     <div className='ser-three-container'>
     <div className='service-three grid-system'>
-        
+        <div className='ser-three-title'>
+            Why MedCare is right for you:
+        </div>
+        <div className='ser-three-secone ser-three-benefits'>
+            <div className='ser-benefits-container'>
+                {/* <img src={} className='ser-benefits-img'/> */}
+                <div className='ser-benefits-title'>24/7 access to patient record</div>.
+                <div className='ser-benefits-para'>dksakjhdkashdkj asdkjjkh sakjhdkajshddsadk skjadh  dasdkasjdkjas sakdjkalsdasdasl;kd</div>.
+            </div>
+        </div>
     </div>
 </div>
   )

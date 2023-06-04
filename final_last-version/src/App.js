832d7bb58e5f93de905e5bf1ebacd4bb79697bef
Mohamad Alkaadi <mohamadalkaadi@gmail.com>
2023-06-04 10:52:47 +0300

    small comit

--- final_last-version/src/App.js
+++ final_last-version/src/App.js
@@ -9,8 +9,8 @@ function App() {
   return (
     <div className="App">
       <NavBar/>
-      {/* <Home/> */}
-      <Services/>
+      <Home/>
+      {/* <Services/> */}
       <Footer/>
     </div>
   );

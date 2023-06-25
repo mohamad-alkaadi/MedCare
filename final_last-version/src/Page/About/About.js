c0770228a42cf375ee290f5858ad9333a00e973e
Mohamad Alkaadi <mohamadalkaadi@gmail.com>
2023-06-25 23:26:36 +0300

    starting about two

--- final_last-version/src/Page/About/About.js
+++ final_last-version/src/Page/About/About.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import AboutOne from './AboutOne'
+import AboutTwo from './AboutTwo'
 const About = () => {
   return (
     <div>
       <AboutOne/>
+      <AboutTwo/>
     </div>
   )
 }
---
+++ final_last-version/src/Page/About/AboutTwo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AboutTwo = () => {
+  return (
+    <div>AboutTwo</div>
+  )
+}
+
+export default AboutTwo

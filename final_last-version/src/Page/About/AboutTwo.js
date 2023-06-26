d3bbd6f225a07d1511aa869cbdeb698aee04f03f
Mohamad Alkaadi <mohamadalkaadi@gmail.com>
2023-06-26 22:19:08 +0300

    about-two test

--- final_last-version/src/Page/About/AboutTwo.js
+++ final_last-version/src/Page/About/AboutTwo.js
@@ -10,12 +10,11 @@ const AboutTwo = () => {
         </div>
         <div className="about-two-text">
           <div className='about-two-title'>
-
+            Our Mission
           </div>
           <div className='about-two-para'>
-            
+            we provide advanced and compassionate care to elderly individuals. We strive to enhance their quality of life by ensuring their physical, emotional, and social needs are met. Our team of highly trained professionals work closely with families and loved ones to create personalized care plans that promote independence and dignity. We are committed to fostering a safe and comfortable environment that promotes overall well-being and happiness.          
           </div>
-
         </div>
       </div>
     </div>

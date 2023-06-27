2b05f0979df85e4e78eb413d1c7fa28573e90959
Mohamad Alkaadi <mohamadalkaadi@gmail.com>
2023-06-27 23:27:58 +0300

    sign in structure

--- final_last-version/src/Page/SignIn.js
+++ final_last-version/src/Page/SignIn.js
@@ -1,10 +1,15 @@
-import React,{useState,useRef} from 'react'
+import React,{useState,useRef,useEffect} from 'react'
 
 function SignIn() {
     const emailRef = useRef();
     const [email, setEmail] = useState('');
     const [validEmail, setValidEmail] = useState(false);
     const [emailFocus, setEmailFocus] = useState(false);
+
+    const [pwd, setPwd] = useState('');
+    const [validPwd, setValidPwd] = useState(false);
+    const [pwdFocus, setPwdFocus] = useState(false);
+
   return (
     <div className='sign-in-container'>
         <div className='sign-in grid-system'>
@@ -12,10 +17,9 @@ function SignIn() {
                 Welcome Back!
             </div>
             <div className='sign-in-email'>
-                <div className='sign-in-title'>
-                    Email
-                </div>
-                <div className='sign-in-field'>
+                    <label htmlFor='signin-email' className="signin-email-label">
+                        Email:
+                    </label>
                     <input 
                         type='email'
                         id='signin-email'
@@ -26,15 +30,21 @@ function SignIn() {
                         onChange={(e) => setEmail(e.target.value)}
                         onFocus={() => setEmailFocus(true)}
                         onBlur={() => setEmailFocus(false)}/>
-
-                
-                </div>
             </div>
+
             <div className='sign-in-password'>
-                <div className='sign-in-title'>
-                    Password
-                </div>
-                <div className='sign-in-field'>
+                <div className='sign-in-password'>
+                    <label htmlFor='signin-password' className="signin-password-label">
+                        Password:
+                    </label>
+                    <input
+                        type='password'
+                        id='signin-password'
+                        onChange={(e) => setPwd(e.target.value)}
+                        required
+                        aria-invalid={validPwd ? "false":"true"}
+                        onFocus={() => setPwdFocus(true)}
+                        onBlur={() => setPwdFocus(false)}/>
 
                 </div>
             </div>

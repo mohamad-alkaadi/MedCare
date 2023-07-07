5d95ed165e1e007ed76d03f47c2e0d02050e7583
Mohamad Alkaadi <mohamadalkaadi@gmail.com>
2023-07-07 23:30:25 +0300

    about new

--- final_last-version/src/Page/About/AboutNew.js
+++ final_last-version/src/Page/About/AboutNew.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import AboutGirl from './about-assets/AboutUsGirl.png'
+import aboutGirlGray from './about-assets/AboutUsGirlGray.png'
 import './about-css/about-new.css'
 function AboutNew() {
   return (
     <div className='about-new-container'>
     <div className='about-new aboutNew-grid-system'>
       
-      <img src={AboutGirl} draggable="false" />
+      <img src={aboutGirlGray} draggable="false" />
       <div className='section-one-para'>
   
       <div className='abo-new-stitle'>About us</div>

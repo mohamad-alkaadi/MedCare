a2bb8f99dd0df476296e92cbf268b7cd8882cab1
Mohamad Alkaadi <mohamadalkaadi@gmail.com>
2023-07-14 22:58:34 +0300

    update links

--- final_last-version/src/Page/Home/SectionOne.js
+++ final_last-version/src/Page/Home/SectionOne.js
@@ -14,7 +14,7 @@ const SectionOne = () => {
             <p className='one-headline'>Where<br/>healthcare<br/>happens<br/>24/7</p>
          
             <p className='one-subheadline'>The best way to protect your loved ones without<br/>worrying about wasting your time</p>
-            <Link className='one-button' to='/signin'>Sign in</Link>
+            <Link className='one-button' to='/MedCare/signin'>Sign in</Link>
           </div>
         </div>
         

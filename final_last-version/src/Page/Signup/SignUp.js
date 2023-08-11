30cab8a7c230f937607ce0ff17e36e1ebe28e224
Mohamad Alkaadi <mohamadalkaadi@gmail.com>
2023-08-11 22:00:48 +0300

    signup icons

--- final_last-version/src/Page/Signup/SignUp.js
+++ final_last-version/src/Page/Signup/SignUp.js
@@ -1,4 +1,7 @@
-import React from 'react'
+import React,{useState,useRef,useEffect} from 'react'
+import './signin-css/signin.css'
+import { FaArrowLeft } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
   return (

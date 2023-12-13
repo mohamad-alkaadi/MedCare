fe7734a4759387e52c5a03b5d77b08e1416140aa
Mohamad Alkaadi <mohamadalkaadi@gmail.com>
2023-12-12 18:31:58 -1200

    comment for viewspy

--- backend/djbackend/chatserver/views.py
+++ backend/djbackend/chatserver/views.py
@@ -5,39 +5,51 @@ from rest_framework.response import Response
 from .models import Server
 from rest_framework.exceptions import ValidationError, AuthenticationFailed
 from django.db.models import Count
-# viewsets is a class that provide CRUD operations
+
+# viewsets is a class that provides CRUD operations
 class ServerListViewSet(viewsets.ViewSet):
     queryset = Server.objects.all()
 
     def list(self, request):
-        category = request.query_params.get("category")  # capture category id
+        # Capture query parameters
+        category = request.query_params.get("category")
         qty = request.query_params.get("qty")
         by_user = request.query_params.get("by_user") == "true"
         by_serverid = request.query_params.get("by_serverid")
         with_num_members = request.query_params.get("with_num_members") == "true"
-        if category:  # if exits
+
+        # Filter queryset based on category if provided
+        if category:
             self.queryset = self.queryset.filter(category__name=category)
 
-        if by_user or by_serverid and not request.user.is_authenticated:
+        # Check authentication for queries based on user
+        if (by_user or by_serverid) and not request.user.is_authenticated:
             raise AuthenticationFailed()
 
+        # Filter queryset by user if 'by_user' is True
         if by_user:
-            user_id =request.user.id
-            self.queryset = self.queryset.filter(member = user_id)
+            user_id = request.user.id
+            self.queryset = self.queryset.filter(member=user_id)
 
+        # Annotate queryset with the number of members if 'with_num_members' is True
         if with_num_members:
-            self.queryset = self.queryset.annotate(num_members = Count("member"))
+            self.queryset = self.queryset.annotate(num_members=Count("member"))
 
+        # Limit the queryset to a specified quantity if 'qty' is provided
         if qty:
             self.queryset = self.queryset[: int(qty)]
 
+        # Filter queryset by server id if 'by_serverid' is provided
         if by_serverid:
             try:
-                self.queryset = self.queryset.filter(id = by_serverid)
+                self.queryset = self.queryset.filter(id=by_serverid)
                 if not self.queryset.exists():
-                    raise ValidationError(detail = f"Server with id {by_serverid} not found")
+                    raise ValidationError(detail=f"Server with id {by_serverid} not found")
             except ValueError:
-                raise ValidationError(detail = f"Server value error")
+                raise ValidationError(detail=f"Server value error")
 
-        serializer = ServerSerializer(self.queryset, many=True, context = {"num_members": with_num_members})
+        # Serialize the queryset using ServerSerializer
+        serializer = ServerSerializer(self.queryset, many=True, context={"num_members": with_num_members})
+
+        # Return the serialized data as a response
         return Response(serializer.data)

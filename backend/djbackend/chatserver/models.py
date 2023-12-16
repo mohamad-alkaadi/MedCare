e19b77caa16c1ce4d0f7895ae5f3983e181da228
Mohamad Alkaadi <mohamadalkaadi@gmail.com>
2023-12-15 20:07:27 -1200

    server banner and server icon

--- backend/djbackend/chatserver/models.py
+++ backend/djbackend/chatserver/models.py
@@ -2,8 +2,15 @@ from django.conf import settings
 from django.db import models
 from django.shortcuts import get_object_or_404
 from django.dispatch import receiver
+
+def server_icon_upload_path(instance, filename):
+    return f"server/{instance.id}/server_icon/{filename}"
+def server_banner_upload_path(instance, filename):
+    return f"server/{instance.id}/server_banner/{filename}"
+
 def category_icon_upload_path(instance, filename):
     return f"category/{instance.id}/category_icon/{filename}"
+
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -16,17 +23,19 @@ class Category(models.Model):
             if existing.icon != self.icon:
                 existing.icon.delete(save=False)
         super(Category, self).save(*args, **kwargs)
-    def __str__(self):
-        return self.name
+
 
 #capture delete event
-@receiver(models.signals.pre_delete, sender="chatserver.Category")
-def category_delete_files(sender, instance, **kwargs):
-    for field in instance._meta.fields:
-        if field.name == "icon":
-            file = getattr(instance, field.name)
-            if file:
-                file.delete(save=False)
+    @receiver(models.signals.pre_delete, sender="chatserver.Category")
+    def category_delete_files(sender, instance, **kwargs):
+        for field in instance._meta.fields:
+            if field.name == "icon":
+                file = getattr(instance, field.name)
+                if file:
+                    file.delete(save=False)
+
+    def __str__(self):
+        return self.name
 
 
 
@@ -46,7 +55,7 @@ class Server(models.Model):
 
     def __str__(self):
         return f"{self.name}-{self.id}"
-    
+
 
 class Channel(models.Model):
     name = models.CharField(max_length=100)
@@ -55,11 +64,31 @@ class Channel(models.Model):
     )
     topic = models.CharField(max_length=100)  # 3lak
     server = models.ForeignKey(Server, on_delete=models.CASCADE, related_name="channel_server")
+    banner = models.ImageField(upload_to=server_banner_upload_path, null=True, blank=True)
+    icon = models.ImageField(upload_to= server_icon_upload_path, null=False, blank=True)
 
-    # how do we want to save our values
+    # # how do we want to save our values
+    # def save(self, *args, **kwargs):
+    #     self.name = self.name.lower()  # save as lower case
+    #     super(Channel, self).save(*args, **kwargs)
     def save(self, *args, **kwargs):
-        self.name = self.name.lower()  # save as lower case
-        super(Channel, self).save(*args, **kwargs)
+        if self.id:
+            existing = get_object_or_404(Category, id=self.id)
+            if existing.icon != self.icon:
+                existing.icon.delete(save=False)
+            if existing.banner != self.banner:
+                existing.banner.delete(save=False)
+
+        super(Category, self).save(*args, **kwargs)
+
+    #capture delete event
+    @receiver(models.signals.pre_delete, sender="chatserver.Channel")
+    def category_delete_files(sender, instance, **kwargs):
+        for field in instance._meta.fields:
+            if field.name == "icon" or field.name == "banner":
+                file = getattr(instance, field.name)
+                if file:
+                    file.delete(save=False)
 
     def __str__(self):
         return self.name

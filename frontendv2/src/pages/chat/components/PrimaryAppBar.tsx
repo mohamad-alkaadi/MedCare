5b08d0a1054cfe67fc4aa5838234f6c29fc1dfe0
Mohamad Alkaadi <mohamadalkaadi@gmail.com>
2023-12-21 18:19:25 +0300

    sitting up drawer

--- frontendv2/src/pages/chat/components/PrimaryAppBar.tsx
+++ frontendv2/src/pages/chat/components/PrimaryAppBar.tsx
@@ -1,15 +1,47 @@
-import { AppBar, Link, Toolbar, Typography } from "@mui/material"
-import React from "react"
+import {
+  AppBar,
+  Box,
+  Drawer,
+  IconButton,
+  Link,
+  Toolbar,
+  Typography,
+  useMediaQuery,
+} from "@mui/material"
+import MenuIcon from "@mui/icons-material/Menu"
+import React, { useEffect, useState } from "react"
 import { useTheme } from "@mui/material/styles"
 
 const PrimaryAppBar = () => {
   const theme = useTheme()
+  const [sideMenu, setSideMenu] = useState(false)
+  // const toggleDrawer =
+  // (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+  //   if (
+  //     event.type === "keydown" &&
+  //     ((event as React.KeyboardEvent).key === "Tab" ||
+  //       (event as React.KeyboardEvent).key === "Shift")
+  //   ) {
+  //     return;
+  //   }
+  //   setSideMenu(open);
+  // };
+
+  const isSmallScreen = useMediaQuery(theme.breakpoints.up("sm"))
+
+  useEffect(() => {
+    if (isSmallScreen && sideMenu) {
+      setSideMenu(false)
+    }
+  }, [isSmallScreen])
 
   return (
     <AppBar
       sx={{
         backgroundColor: theme.palette.background.default,
         borderBottom: `1px solid ${theme.palette.divider}`,
+        // what ever the drawer is + 2 meaning make it on top of it
+        zIndex: (theme) => theme.zIndex.drawer + 2,
       }}
     >
       <Toolbar
@@ -19,6 +51,42 @@ const PrimaryAppBar = () => {
           minHeight: theme.primaryAppBar.height,
         }}
       >
+        <Box sx={{ display: { xs: "block", sm: "none" } }}>
+          {/* <IconButton
+            color="inherit"
+            onClick={() => toggleDrawer(true)}
+            aria-label="open drawer"
+            edge="start"
+            sx={{ marginRight: 0.5 }}
+          > */}
+          <IconButton
+            color="inherit"
+            onClick={() => setSideMenu(!sideMenu)}
+            aria-label="open drawer"
+            edge="start"
+            sx={{ marginRight: 0.5 }}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Box>
+
+        {/* <Drawer
+          anchor="left"
+          open={sideMenu}
+          onClose={() => toggleDrawer(false)}
+        > */}
+        <Drawer
+          anchor="left"
+          open={sideMenu}
+          onClose={() => setSideMenu(false)}
+        >
+          {[...Array(100)].map((_, i) => (
+            <Typography key={i} paragraph>
+              {i + 1}
+            </Typography>
+          ))}
+        </Drawer>
+
         <Link underline="none" color="inherit" href="/chat/">
           <Typography
             noWrap
--- frontendv2/src/pages/chat/theme/theme.tsx
+++ frontendv2/src/pages/chat/theme/theme.tsx
@@ -1,4 +1,4 @@
-import { createTheme } from "@mui/material"
+import { createTheme, responsiveFontSizes } from "@mui/material"
 
 declare module "@mui/material/styles" {
   interface Theme {
@@ -15,6 +15,9 @@ declare module "@mui/material/styles" {
 
 export const createMuiTheme = () => {
   let theme = createTheme({
+    typography: {
+      fontFamily: [`'IBM Plex Sans', sans-serif`].join(","),
+    },
     primaryAppBar: {
       height: 50,
     },
@@ -27,6 +30,7 @@ export const createMuiTheme = () => {
       },
     },
   })
+  theme = responsiveFontSizes(theme)
   return theme
 }
 
